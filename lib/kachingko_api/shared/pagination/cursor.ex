7b71138b92e6134bae8c5c8282ebc7094cba6ce6
Bry Madrid <hi@braynm.com>
2025-09-20 20:26:05 +0800

    fix(sobelow): sobelow unsafe String.to_atom conversion

--- lib/kachingko_api/shared/pagination/cursor.ex
+++ lib/kachingko_api/shared/pagination/cursor.ex
@@ -30,7 +30,15 @@ defmodule KachingkoApi.Shared.Pagination.Cursor do
 
   def deserialize_position(position) do
     Map.new(position, fn {key, value} ->
-      {String.to_atom(key), deserialize_value(value)}
+
+    atom_key = case key do
+      key when key in ~W[sale_date id] ->
+        String.to_existing_atom(key)
+      _ ->
+        raise "Invalid cursor field: #{key}"
+    end
+
+    {atom_key, deserialize_value(value)}
     end)
   end
 
